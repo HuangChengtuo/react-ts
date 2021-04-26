52b208f0e71d77236da2732dee21a76a96f56389
HuangChengtuo <hct564117373@gmail.com>
2021-04-26 23:51:17 +0800

    update

--- src/pages/index/Index.tsx
+++ src/pages/index/Index.tsx
@@ -1,18 +1,23 @@
 import './Index.scss';
-import { ClockCircleOutlined } from '@ant-design/icons'
-import {NavLink} from "react-router-dom";
+import { ClockCircleOutlined, CalculatorOutlined } from '@ant-design/icons'
+import { NavLink } from "react-router-dom";
 
 export default function Index() {
   return (
     <div id="home-page">
       <h1>黄秤砣的工具箱</h1>
-      <NavLink to="/unixTime" className="grid">
-        <div> </div>
-        <div className="card">
+      <div className="grid">
+        <div></div>
+
+        <NavLink to="/unixTime" className="card">
           <h2>unix时间戳计算机</h2>
           <ClockCircleOutlined style={{ fontSize: '192px' }} />
-        </div>
-      </NavLink>
+        </NavLink>
+        <NavLink to="/hex" className="card">
+          <h2>进制转换</h2>
+          <CalculatorOutlined style={{ fontSize: '192px' }} />
+        </NavLink>
+      </div>
     </div>
   );
 }

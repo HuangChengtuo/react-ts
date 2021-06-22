d61c8ea76885e1d00793f527c8b6cb5547b52800
chengtuo <chengtuo@dtstack.com>
2021-06-22 18:02:24 +0800

    redux

--- src/pages/redux/a.tsx
+++ src/pages/redux/a.tsx
@@ -1,18 +1,24 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import type { State } from '@/store'
 import { Button } from 'antd'
 
-export default function ReduxA() {
+export default function ReduxA () {
   const count = useSelector<State, number>(state => state.count)
   const dispatch = useDispatch()
   const add = () => {
     dispatch({ type: 'count', payload: count + 1 })
   }
+
+  const router = useHistory()
+
   return (
-    <div className="redux-a">
-      <h1>redux a</h1>
-      count:{count}
+    <div className="redux-a page-1200">
+      <h1>redux A</h1>
+      <div>count:{count}</div>
       <Button onClick={add}>add</Button>
+      <br />
+      <Button onClick={() => {router.push('/redux/b?hct=123')}}>go to B</Button>
     </div>
   )
 }
--- src/pages/redux/b.tsx
+++ src/pages/redux/b.tsx
@@ -1,18 +1,23 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 import type { State } from '@/store'
 import { Button } from 'antd'
 
-export default function ReduxA() {
+export default function ReduxA () {
   const count = useSelector<State, number>(state => state.count)
   const dispatch = useDispatch()
   const minus = () => {
     dispatch({ type: 'count', payload: count - 1 })
   }
+
+
   return (
-    <div className="redux-b">
-      <h1>redux b</h1>
-      count:{count}
+    <div className="redux-b page-1200">
+      <h1>redux B</h1>
+      <div>count:{count}</div>
       <Button onClick={minus}>minus</Button>
+      <br />
+      <NavLink to="/redux/a">go to A</NavLink>
     </div>
   )
 }

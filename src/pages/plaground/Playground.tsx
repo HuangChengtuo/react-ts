4b4b6853888d9e07f5754d4a9c07eadcd06aeec7
chengtuo <chengtuo@dtstack.com>
2021-06-22 20:24:52 +0800

    router

--- src/pages/plaground/Playground.tsx
+++ src/pages/plaground/Playground.tsx
@@ -1,35 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './Playground.scss'
+import BlockA from './BlockA'
 
-import { Button, Select, DatePicker } from 'antd'
-
-const Option = Select.Option
-
-export default function Playground (): React.ReactElement {
-  const [text] = useState('asd')
-  useEffect(() => {
-    console.log(text)
-  }, [])
-  const btnFn = (e: React.MouseEvent) => {
-    console.log(e)
-  }
-
-  const selectFn = (e: string, arr) => {
-    console.log(arr)
-  }
-
-  const dateFn = (e) => {
-    console.log(e)
-  }
-
-  return (
-    <div id="playground">
-      <DatePicker onChange={dateFn} />
-      <Button onClick={btnFn}>asd</Button>
-      <Select defaultValue="lucy" onChange={selectFn}>
-        <Option value="jack">Jack</Option>
-        <Option value="lucy">Lucy</Option>
-      </Select>
+export default class Playground extends React.Component {
+  render () {
+    return <div className="page-1200">
+      <BlockA text="hello" />
     </div>
-  );
+  }
 }

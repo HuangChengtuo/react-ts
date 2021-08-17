af67f98f5a6710da139050a200c6fddbc49d2e2a
chengtuo <chengtuo@dtstack.com>
2021-08-17 16:52:29 +0800

    refactor index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,51 @@
-import { ClockCircleOutlined, CalculatorOutlined, YoutubeOutlined, CalendarOutlined } from '@ant-design/icons'
+import { ClockCircleOutlined, CalculatorOutlined, YoutubeOutlined, CalendarOutlined, BgColorsOutlined } from '@ant-design/icons'
 import NavLink from '@/components/Link'
 
+interface Card {
+  title: string,
+  link: string,
+  Icon: React.ForwardRefExoticComponent<any>
+}
+
 export default function Index () {
+  const cards: Card[] = [
+    {
+      title: '新番时间表',
+      link: 'http://www.huangchengtuo.com/bangumi',
+      Icon: CalendarOutlined
+    },
+    {
+      title: 'unix时间戳计算器',
+      link: '/unixTime',
+      Icon: ClockCircleOutlined
+    },
+    {
+      title: '进制转换',
+      link: '/hex',
+      Icon: CalculatorOutlined
+    },
+    {
+      title: 'RGB 转换器',
+      link: '/rgb',
+      Icon: BgColorsOutlined
+    },
+    {
+      title: '新番 JSON 生成',
+      link: '/bangumi2json',
+      Icon: YoutubeOutlined
+    }
+  ]
+
   return (
     <div id="home-page">
       <h1>黄秤砣的工具箱</h1>
       <div className="grid">
-        <a href="http://www.huangchengtuo.com/bangumi" className="card">
-          <h2>新番时间表</h2>
-          <CalendarOutlined style={{ fontSize: '192px' }} />
-        </a>
-        <NavLink to="/unixTime" className="card">
-          <h2>unix时间戳计算器</h2>
-          <ClockCircleOutlined style={{ fontSize: '192px' }} />
-        </NavLink>
-        <NavLink to="/hex" className="card">
-          <h2>进制转换</h2>
-          <CalculatorOutlined style={{ fontSize: '192px' }} />
-        </NavLink>
-        <NavLink to="/bangumi2json" className="card">
-          <h2>新番 JSON 生成</h2>
-          <YoutubeOutlined style={{ fontSize: '192px' }} />
-        </NavLink>
+        {cards.map(item =>
+          <NavLink key={item.title} to={item.link} className="card">
+            <h2>{item.title}</h2>
+            <item.Icon style={{ fontSize: '192px' }} />
+          </NavLink>
+        )}
       </div>
     </div>
   );

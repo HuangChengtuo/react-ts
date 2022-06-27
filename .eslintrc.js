3af7d1232d498305aaf8b2c8e232a8db84fb5821
chengtuo <chengtuo@dtstack.com>
2022-06-27 17:39:24 +0800

    :wrench: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,20 @@
 module.exports = {
-  'extends': ['next'],
+  'extends': [
+    'next',
+    "plugin:@typescript-eslint/recommended"
+  ],
   'rules': {
     'eol-last': 1,
     'eqeqeq': 2,
     'no-var': 2,
     'prefer-const': 2,
     'quotes': [1, 'single'],
-    '@next/next/no-img-element': 0
+    '@next/next/no-img-element': 0,
+    '@typescript-eslint/member-delimiter-style': [1, {
+      multiline: { delimiter: 'comma', requireLast: false },
+      singleline: { delimiter: 'comma', requireLast: false }
+    }],
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-explicit-any': 0
   }
 }
--- components/Link.tsx
+++ components/Link.tsx
@@ -1,4 +1,5 @@
 import NextLink from 'next/link'
+import React from 'react';
 
 interface Props {
   to: string,

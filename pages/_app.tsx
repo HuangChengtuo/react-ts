c874488eedcda4725cf41e135ce53f471bd18ae2
chengtuo <chengtuo@dtstack.com>
2023-01-30 15:36:10 +0800

    style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import Layout from '@/layouts'
 import { Provider } from 'react-redux'
 import store from '../store'
 
-// import 'antd/dist/reset.css'
 import '@/components/components.scss'
 import '@/layouts/layouts.scss'
 import '@/css/global.scss'
@@ -14,6 +13,7 @@ export default function App ({ Component, pageProps, router }: AppProps) {
   return <>
     <Head>
       <title>黄秤砣的工具箱</title>
+      <link rel="icon" type="image/png" href="/favicon.png" />
     </Head>
     <Provider store={store}>
       <Layout router={router}>
